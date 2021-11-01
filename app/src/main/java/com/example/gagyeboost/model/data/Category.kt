89b8efed1b432c108ba5beb5a248fc6b905627b4
kim0hoon <gns9460@gmail.com>
2021-11-01 16:17:27 +0900

    feat: init data class Category

---
+++ app/src/main/java/com/example/gagyeboost/model/data/Category.kt
@@ -0,0 +1,15 @@
+package com.example.gagyeboost.model.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "category")
+data class Category(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id:Int=0,
+
+    @ColumnInfo(name = "emoji")
+    val emoji:String
+)
