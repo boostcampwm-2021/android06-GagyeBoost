5fc1f1309ea2703e481f6cff778c2aef53fa9d3a
7hong13 <ledi000@naver.com>
2021-11-02 01:44:56 +0900

    fix: _binding not initialized error 해결

--- app/src/main/java/com/example/gagyeboost/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/gagyeboost/ui/base/BaseFragment.kt
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment
 abstract class BaseFragment<T : ViewDataBinding>(private val layoutId: Int) : Fragment() {
 
     private var _binding: T? = null
-    protected val binding = _binding!!
+    protected val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
