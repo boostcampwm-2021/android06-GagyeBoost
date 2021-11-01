e0274ff54747d85029c02171247608e30839f62e
kim0hoon <gns9460@gmail.com>
2021-11-01 16:15:46 +0900

    feat: init data class AccountBook

---
+++ app/src/main/java/com/example/gagyeboost/model/data/AccountBook.kt
@@ -0,0 +1,39 @@
+package com.example.gagyeboost.model.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "account_book")
+data class AccountBook(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int = 0,
+
+    @ColumnInfo(name = "money")
+    val money: UByte,
+
+    @ColumnInfo(name = "category")
+    val category: Int,
+
+    @ColumnInfo(name = "latitude")
+    val latitude: Float,
+
+    @ColumnInfo(name = "longitude")
+    val longitude: Float,
+
+    @ColumnInfo(name = "address")
+    val address: String,
+
+    @ColumnInfo(name = "content")
+    val content: String,
+
+    @ColumnInfo(name = "year")
+    val year: Int,
+
+    @ColumnInfo(name = "month")
+    val month: Int,
+
+    @ColumnInfo(name = "day")
+    val day: Int
+)
