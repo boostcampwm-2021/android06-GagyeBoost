2824e37ec5df66f14833f16f4cc604b8cb093170
kim0hoon <gns9460@gmail.com>
2021-11-01 23:09:08 +0900

    test: getMonthIncome, getMonthExpense, addCategoryData, isExistCategoryName, getCategoryData, updateAccountData test 코드 추가

--- app/src/androidTest/java/com/example/gagyeboost/DatabaseTest.kt
+++ app/src/androidTest/java/com/example/gagyeboost/DatabaseTest.kt
@@ -146,6 +146,24 @@ class DatabaseTest {
     }
 
     @Test
+    fun addCategory_categorySize_Four() {
+        //Given
+        accountBookDao.addCategoryData(
+            Category(
+                id = 4,
+                categoryName = "의료",
+                emoji = "\uD83C\uDFE5"
+            )
+        )
+
+        //When
+        val size = accountBookDao.getCategoryAllData().size
+
+        //Then
+        assertThat(size, `is`(4))
+    }
+
+    @Test
     fun getAccountBookAllData_isContainsAll_True() {
         //Given
 
@@ -154,5 +172,78 @@ class DatabaseTest {
 
         //Then
         assertThat(dataList.containsAll(baseAccountBookList), `is`(true))
+    }
+
+    @Test
+    fun getOctoberIncome_income_zero() {
+        //Given
+
+        //When
+        val income = accountBookDao.getMonthIncome(2021, 10)
+
+        //Then
+        assertThat(income, `is`(0))
+    }
+
+    @Test
+    fun getOctoberExpense_expense_sixThousand() {
+        //Given
+
+        //When
+        val expense = accountBookDao.getMonthExpense(2021, 10)
+
+        //Then
+        assertThat(expense, `is`(6000))
+    }
+
+    @Test
+    fun isExistCategoryName_isExist_true() {
+        //Given
+
+        //When
+        val isExist = accountBookDao.isExistCategoryName("교통")
+
+        //Then
+        assertThat(isExist, `is`(true))
+    }
+
+    @Test
+    fun isExistCategoryName_isExist_false() {
+        //Given
+
+        //When
+        val isExist = accountBookDao.isExistCategoryName("의료")
+
+        //Then
+        assertThat(isExist, `is`(false))
+    }
+
+    @Test
+    fun getCategoryData_isSameCategory_true() {
+        //Given
+
+        //When
+        val category = accountBookDao.getCategoryData(1)
+
+        //Then
+        assertThat(category == baseCategoryList[0], `is`(true))
+    }
+
+    @Test
+    fun updateAccountData_isUpdated_true() {
+        //Given
+        val updatedData = baseAccountBookList[1].copy()
+        updatedData.money = 100000
+
+        //When
+        accountBookDao.updateAccountBookData(updatedData)
+        val isUpdated = (accountBookDao.getSearchData(
+            startMoney = 100000,
+            endMoney = 100000,
+            categoryList = listOf(1, 2, 3)
+        ))[0] == updatedData
+
+        //Then
+        assertThat(isUpdated, `is`(true))
     }
 }
