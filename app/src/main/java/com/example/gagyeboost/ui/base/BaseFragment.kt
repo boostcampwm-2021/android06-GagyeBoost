7ecf74a06f7f410eabcf73349811edcc78b96370
신재혁 <shs0985@gmail.com>
2021-11-01 14:49:48 +0900

    feat: BaseActivity, BaseFragment 추가

---
+++ app/src/main/java/com/example/gagyeboost/ui/base/BaseFragment.kt
@@ -0,0 +1,34 @@
+package com.example.gagyeboost.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding>(private val layoutId: Int) : Fragment() {
+
+    private var _binding: T? = null
+    protected val binding = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        return _binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
