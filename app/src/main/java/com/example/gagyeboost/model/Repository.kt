97c45b61c7e73e25c98db6ca45110288d5ee41c7
신재혁 <shs0985@gmail.com>
2021-11-02 17:11:41 +0900

    feat: repository에 지정 월 수입/지출 데이터 가져오기 함수 구현

--- app/src/main/java/com/example/gagyeboost/model/Repository.kt
+++ app/src/main/java/com/example/gagyeboost/model/Repository.kt
@@ -1,6 +1,20 @@
 package com.example.gagyeboost.model
 
+import com.example.gagyeboost.model.data.AccountBook
+import com.example.gagyeboost.model.data.Category
 import com.example.gagyeboost.model.local.AccountBookDAO
 
 class Repository(private val accountBookDao: AccountBookDAO) {
+
+    suspend fun getMonthIncome(year: Int, month: Int) = accountBookDao.getMonthIncome(year, month).toString()
+
+    suspend fun addAccountBookData(accountBook: AccountBook) {
+        accountBookDao.addAccountBookData(accountBook)
+    }
+
+    suspend fun addCategoryData(category: Category) {
+        accountBookDao.addCategoryData(category)
+    }
+
+    suspend fun getMonthExpense(year: Int, month: Int) = accountBookDao.getMonthExpense(year, month).toString()
 }
