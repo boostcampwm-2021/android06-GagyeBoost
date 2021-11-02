b6fcf00460344ee0a5793d08ef478a9c9ab2bc23
신재혁 <shs0985@gmail.com>
2021-11-02 18:23:14 +0900

    feat: 홈화면 수입, 지출, 남은 금액 보여주기

--- app/src/main/java/com/example/gagyeboost/model/Repository.kt
+++ app/src/main/java/com/example/gagyeboost/model/Repository.kt
@@ -1,12 +1,14 @@
 package com.example.gagyeboost.model
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.gagyeboost.model.data.AccountBook
 import com.example.gagyeboost.model.data.Category
 import com.example.gagyeboost.model.local.AccountBookDAO
 
 class Repository(private val accountBookDao: AccountBookDAO) {
 
-    suspend fun getMonthIncome(year: Int, month: Int) = accountBookDao.getMonthIncome(year, month).toString()
+    suspend fun getMonthIncome(year: Int, month: Int) = accountBookDao.getMonthIncome(year,month)
 
     suspend fun addAccountBookData(accountBook: AccountBook) {
         accountBookDao.addAccountBookData(accountBook)
@@ -16,5 +18,5 @@ class Repository(private val accountBookDao: AccountBookDAO) {
         accountBookDao.addCategoryData(category)
     }
 
-    suspend fun getMonthExpense(year: Int, month: Int) = accountBookDao.getMonthExpense(year, month).toString()
+    suspend fun getMonthExpense(year: Int, month: Int) = accountBookDao.getMonthExpense(year, month)
 }
--- app/src/main/java/com/example/gagyeboost/ui/MainViewModel.kt
+++ app/src/main/java/com/example/gagyeboost/ui/MainViewModel.kt
@@ -1,9 +1,57 @@
 package com.example.gagyeboost.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.gagyeboost.model.Repository
 import kotlinx.coroutines.launch
+import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainViewModel(private val repository: Repository) : ViewModel() {
+
+    private val _income = MutableLiveData<String>()
+    val income get() = _income
+
+    private val _expense = MutableLiveData<String>()
+    val expense get() = _expense
+
+    private val _result = MutableLiveData<String>()
+    val result get() = _result
+
+    private val formatter = DecimalFormat("###,###")
+
+    private val dateFormatter = SimpleDateFormat("yyyy MM dd", Locale.getDefault())
+    private val date =
+        dateFormatter.format(Date(System.currentTimeMillis())).split(" ").map { it.toInt() }
+
+    fun getMonthIncome() {
+        viewModelScope.launch {
+            repository.getMonthIncome(date[0], date[1])?.let {
+                _income.postValue(formatter.format(it) + "원")
+            } ?: _income.postValue("0")
+        }
+    }
+
+    fun getMonthExpense() {
+        viewModelScope.launch {
+            repository.getMonthExpense(date[0], date[1])?.let {
+                _expense.postValue(formatter.format(it) + "원")
+            } ?: _expense.postValue("0원")
+        }
+    }
+
+    fun setTotalMoney() {
+        viewModelScope.launch {
+            val income = repository.getMonthIncome(date[0], date[1])
+            val expense = repository.getMonthExpense(date[0], date[1])
+
+            val result = expense?.let {
+                formatter.format(income?.minus(it) ?: 0) + "원"
+            } ?: formatter.format(income ?: 0) + "원"
+
+            _result.postValue(result)
+        }
+    }
 }
