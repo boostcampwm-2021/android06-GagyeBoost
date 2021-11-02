44cae35f99bfb4dfba37b5515d8c852e3c45b14f
anhahyoun <dksgkgus@gmail.com>
2021-11-02 19:13:49 +0900

    feat: NumberPicker 선택 시 month 바꾸기

--- app/src/main/java/com/example/gagyeboost/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/gagyeboost/ui/home/HomeFragment.kt
@@ -5,10 +5,10 @@ import android.view.Gravity
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
+import com.example.gagyeboost.R
 import com.example.gagyeboost.databinding.FragmentHomeBinding
 import com.example.gagyeboost.ui.base.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import com.example.gagyeboost.R
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
@@ -22,14 +22,30 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
         initView()
 
         binding.tvMonth.setOnClickListener {
-            val dialog = NumberPickerDialog(binding.root.context)
-            dialog.window?.setGravity(Gravity.TOP)
-            dialog.show()
+            setDialog()
+        }
+    }
+
+    private fun setDialog() {
+
+        val dialog = NumberPickerDialog(binding.root.context)
+        dialog.window?.setGravity(Gravity.TOP)
+        dialog.show()
+
+        dialog.binding.tvAgree.setOnClickListener {
+            val year = dialog.binding.npYear.value
+            val month = dialog.binding.npMonth.value
+            viewModel.setYear(year)
+            viewModel.setMonth(month)
+            dialog.dismiss()
+        }
+        dialog.binding.tvCancel.setOnClickListener {
+            dialog.dismiss()
         }
     }
 
     private fun initView() {
-
+        binding.viewModel = viewModel
         with(binding.rvCalendar) {
             adapter = customCalendarAdapter
             addItemDecoration(
--- app/src/main/java/com/example/gagyeboost/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/gagyeboost/ui/home/HomeViewModel.kt
@@ -1,10 +1,25 @@
 package com.example.gagyeboost.ui.home
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.example.gagyeboost.model.Repository
+import java.util.*
 
 class HomeViewModel(val repository: Repository): ViewModel() {
 
     private val _month = MutableLiveData<Int>()
+    val month = Transformations.map(_month) { "${it}월" }
+
+    init {
+        val currentMonth = Calendar.getInstance().get(Calendar.MONTH)
+        setMonth(currentMonth)
+    }
+
+    fun setMonth(month: Int) {
+        _month.value = month
+    }
+
+    fun setYear(year: Int) {}
+
 }
