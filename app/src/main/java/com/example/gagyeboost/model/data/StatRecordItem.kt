322e4ca2ae34eb00ca77b3ed24ad0faf46de7c64
7hong13 <ledi000@naver.com>
2021-11-08 22:56:30 +0900

    feat: StstResultAdapter 추가

---
+++ app/src/main/java/com/example/gagyeboost/model/data/StatRecordItem.kt
@@ -0,0 +1,8 @@
+package com.example.gagyeboost.model.data
+
+data class StatRecordItem(
+    val categoryIcon: String,
+    val categoryName: String,
+    val percentage: Int,
+    val totalMoney: Int
+)
