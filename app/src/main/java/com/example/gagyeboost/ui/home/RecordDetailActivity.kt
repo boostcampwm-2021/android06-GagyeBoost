5537ba402e8d0feceffeb59021ea297fed5c9b52
7hong13 <ledi000@naver.com>
2021-11-09 18:24:46 +0900

    feat: RecordDetailActivity 추가

---
+++ app/src/main/java/com/example/gagyeboost/ui/home/RecordDetailActivity.kt
@@ -0,0 +1,15 @@
+package com.example.gagyeboost.ui.home
+
+import android.os.Bundle
+import com.example.gagyeboost.R
+import com.example.gagyeboost.databinding.ActivityRecordDetailBinding
+import com.example.gagyeboost.ui.base.BaseActivity
+
+class RecordDetailActivity :
+    BaseActivity<ActivityRecordDetailBinding>(R.layout.activity_record_detail) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
