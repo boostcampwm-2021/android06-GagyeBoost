9cca86a6e13775cc174e73888e0831f474500006
신재혁 <shs0985@gmail.com>
2021-11-10 00:22:07 +0900

    feat: BottomSheetDialogFragment 및 Adapter 추가

---
+++ app/src/main/java/com/example/gagyeboost/ui/home/selectPosition/AddressAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.gagyeboost.ui.home.selectPosition
+
+import android.location.Address
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gagyeboost.databinding.ItemAddressBinding
+
+class AddressAdapter : ListAdapter<Address, AddressAdapter.AddressViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddressViewHolder {
+        val binding = ItemAddressBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return AddressViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AddressViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class AddressViewHolder(private val binding: ItemAddressBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Address) {
+            binding.tvAddress.text = item.getAddressLine(0)
+        }
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<Address>() {
+            override fun areItemsTheSame(oldItem: Address, newItem: Address) =
+                oldItem.featureName == newItem.featureName
+
+            override fun areContentsTheSame(oldItem: Address, newItem: Address) =
+                oldItem.equals(newItem)
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gagyeboost/ui/home/selectPosition/AddressResultFragment.kt
@@ -0,0 +1,41 @@
+package com.example.gagyeboost.ui.home.selectPosition
+
+import android.location.Address
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gagyeboost.databinding.DialogBottomAddressResultBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class AddressResultFragment(private val list: List<Address>) : BottomSheetDialogFragment() {
+
+    private var _binding: DialogBottomAddressResultBinding? = null
+    private val binding get() = _binding!!
+    private val adapter: AddressAdapter by lazy {
+        AddressAdapter().apply {
+            submitList(list)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding =
+            DialogBottomAddressResultBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rvAddress.adapter = adapter
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
