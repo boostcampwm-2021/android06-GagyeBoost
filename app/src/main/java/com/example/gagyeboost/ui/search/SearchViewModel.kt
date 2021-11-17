a938d8bc4b22f64eb87aeb021c71fd9fcf264b3c
kim0hoon <gns9460@gmail.com>
2021-11-17 21:54:56 +0900

    feat: keyword, money data binding

--- app/src/main/java/com/example/gagyeboost/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/gagyeboost/ui/search/SearchViewModel.kt
@@ -12,35 +12,36 @@ import kotlinx.coroutines.launch
 import java.util.*
 
 class SearchViewModel(private val repository: Repository) : ViewModel() {
-    private val _keyword = MutableLiveData("")
-    val keyword: LiveData<String> = _keyword
 
-    /*
-    private val _filter = MutableLiveData(DEFAULT_FILTER)
-    val filter: LiveData<Filter> = _filter
-
-     */
+    val keyword = MutableLiveData("")
 
     private val _startYear = MutableLiveData(Calendar.getInstance().get(Calendar.YEAR))
-    val startYear:LiveData<Int> = _startYear
+    val startYear: LiveData<Int> = _startYear
 
-    private val _startMonth = MutableLiveData(Calendar.getInstance().get(Calendar.MONTH)+1)
-    val startMonth:LiveData<Int> = _startMonth
+    private val _startMonth = MutableLiveData(Calendar.getInstance().get(Calendar.MONTH) + 1)
+    val startMonth: LiveData<Int> = _startMonth
 
     private val _startDay = MutableLiveData(1)
-    val startDay:LiveData<Int> = _startDay
+    val startDay: LiveData<Int> = _startDay
 
     private val _endYear = MutableLiveData(Calendar.getInstance().get(Calendar.YEAR))
-    val endYear:LiveData<Int> = _endYear
+    val endYear: LiveData<Int> = _endYear
 
-    private val _endMonth = MutableLiveData(Calendar.getInstance().get(Calendar.MONTH)+1)
-    val endMonth:LiveData<Int> = _endMonth
+    private val _endMonth = MutableLiveData(Calendar.getInstance().get(Calendar.MONTH) + 1)
+    val endMonth: LiveData<Int> = _endMonth
 
-    private val _endDay = MutableLiveData(Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH))
-    val endDay:LiveData<Int> = _endDay
+    private val _endDay =
+        MutableLiveData(Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH))
+    val endDay: LiveData<Int> = _endDay
 
-    fun loadFilterData(){
-        viewModelScope.launch{
+    private val _startMoney = MutableLiveData(0)
+    val startMoney: LiveData<Int> = _startMoney
+
+    private val _endMoney = MutableLiveData(1000000)
+    val endMoney: LiveData<Int> = _endMoney
+
+    fun loadFilterData() {
+        viewModelScope.launch {
             /*
             val deferredDataList=async{
                 repository.loadFilterDataWithKeyword(filter.value?:DEFAULT_FILTER,keyword.value?:"")
