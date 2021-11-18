03d1acf39b47dfb9b38b2520b260901a798d36c4
7hong13 <ledi000@naver.com>
2021-11-18 14:57:44 +0900

    feat: SplashActivity StatusBar 및 BackgroundColor 수정

--- app/src/main/java/com/example/gagyeboost/ui/intro/IntroActivity.kt
+++ app/src/main/java/com/example/gagyeboost/ui/intro/IntroActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.animation.AnimationUtils
+import androidx.core.content.ContextCompat
 import androidx.core.os.postDelayed
 import com.example.gagyeboost.R
 import com.example.gagyeboost.databinding.ActivityIntroBinding
@@ -14,6 +15,9 @@ class IntroActivity : BaseActivity<ActivityIntroBinding>(R.layout.activity_intro
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.root.setBackgroundColor(ContextCompat.getColor(this, R.color.baseColor))
+        window.statusBarColor = ContextCompat.getColor(this, R.color.baseColor)
 
         val bounce = AnimationUtils.loadAnimation(this, R.anim.overshoot_from_top_to_bottom)
         val leftToRight = AnimationUtils.loadAnimation(this, R.anim.appear_from_left_to_right)
