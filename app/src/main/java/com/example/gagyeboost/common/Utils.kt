760c26c1eb2e9002158237a8e04fc624e07309cf
kim0hoon <gns9460@gmail.com>
2021-11-24 19:23:14 +0900

    feat: 위도 경도 범위 체크 parameter 타입 추가

--- app/src/main/java/com/example/gagyeboost/common/Utils.kt
+++ app/src/main/java/com/example/gagyeboost/common/Utils.kt
@@ -3,6 +3,7 @@ package com.example.gagyeboost.common
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.TextView
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.textfield.TextInputEditText
 import java.text.SimpleDateFormat
 import java.util.*
@@ -38,3 +39,6 @@ fun intToStringDate(year: Int, month: Int, day: Int): String {
 
 fun isValidPosition(lat: Double?, lng: Double?) =
     lat != null && lat in (MIN_LAT..MAX_LAT) && lng != null && lng in (MIN_LNG..MAX_LNG)
+
+fun isValidPosition(latLng: LatLng?) =
+    latLng != null && isValidPosition(latLng.latitude, latLng.longitude)
